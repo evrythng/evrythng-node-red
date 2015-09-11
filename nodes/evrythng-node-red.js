91cb8ec32f3f809fd0449b3a33fdb9a8b14ab4de
Jonathan Carter <jc@glimworm.com>
2015-09-11 11:27:58 +0200

    I adapted the code to allow the user to send the thing id , the key and the value in the payload thus making it compatible with dynamic flows in node red.

--- nodes/evrythng-node-red.js
+++ nodes/evrythng-node-red.js
@@ -3,11 +3,21 @@ module.exports = function(RED) {
 	function thngNode(config) {
 		RED.nodes.createNode(this,config);
 		var evrythngBaseUrl = "https://api.evrythng.com";
-		var propUri = "/thngs/" + config.thng + "/properties/" + config.property;
+		//var propUri = "/thngs/" + config.thng + "/properties/" + config.property;
+		var propUri = "/thngs/" + config.thng + "/properties";
 		var fullUrl = evrythngBaseUrl + propUri;
 		this.on('input', function(msg) {
 			var node = this;
 			var util = require('util');
+			
+			/* override the key and value and thing with the payload if entered */
+			var key = (msg && msg.payload && msg.payload.key) ? msg.payload.key : config.property;
+			var value = (msg && msg.payload && msg.payload.value) ? msg.payload.value : config.value;
+			var thng = (msg && msg.payload && msg.payload.thng) ? msg.payload.thng : config.thng;
+
+			var propUri = "/thngs/" + thng + "/properties";
+			var fullUrl = evrythngBaseUrl + propUri;
+			
 			
 			var options = {
 				url: propUri,
@@ -24,11 +34,12 @@ module.exports = function(RED) {
 					node.send(err);                            
 				}
 			}
+			
 
 			// PUT requests will have a payload
 			node.log("Requesting " + config.method.toUpperCase() + " " + propUri);	
 			if(config.method === 'put') {
-				options.data = [{"value" : config.propertyValue}];
+				options.data = [{"key" : key, "value" : value }];
 				node.log("With property value " + util.inspect(options.data));
 			}
 			EVT.api(options);
